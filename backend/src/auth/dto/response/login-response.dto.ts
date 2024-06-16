a2c5ff7c99ffea72320ea42c1eb0e8c6ab97e39a
Jan Szczuryk <jan.szczuryk@gmail.com>
2024-06-16 23:12:22 +0200

    feat(backend): include account data in auth login endpoint

--- backend/src/auth/dto/response/login-response.dto.ts
+++ backend/src/auth/dto/response/login-response.dto.ts
@@ -1,7 +1,11 @@
-export class LoginResponseDto {
+import { AccountResponseDto } from '@module/account/dto/response';
+import { Account } from '@module/account/entities/account.entity';
+
+export class LoginResponseDto extends AccountResponseDto {
   public readonly jwtToken: string;
 
-  public constructor(props: { jwtToken: string }) {
-    this.jwtToken = props.jwtToken;
+  public constructor(account: Account & { jwtToken: string }) {
+    super(account);
+    this.jwtToken = account.jwtToken;
   }
 }
